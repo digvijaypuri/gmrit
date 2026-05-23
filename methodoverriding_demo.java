a12ed5aa1ee0f022ccdd4c697d9157f411b1b0af
digvijaypuri <41981248+digvijaypuri@users.noreply.github.com>
2026-05-23 15:47:02 +0530

    Add files via upload

---
+++ methodoverriding_demo.java
@@ -0,0 +1,21 @@
+public class methodoverriding_demo {
+//WAP to implement the Method Overriding Using Inheritance (Simple)
+    // Parent class
+    static class Parent {
+        void display() {
+            System.out.println("This is the parent class.");
+        }
+    }
+    // Child class that overrides the display method
+    static class Child extends Parent {
+        @Override
+        void display() {
+            System.out.println("This is the child class, overriding the parent class method.");
+        }}
+    public static void main(String[] args) {
+        // Create an object of the Child class
+        Child childObject = new Child();
+        // Call the display method, which will invoke the overridden method in the Child class
+        childObject.display();
+    }
+}
